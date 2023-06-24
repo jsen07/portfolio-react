976fa1b37ec99fd935837ef1a7f024e384bff540
unknown <jayssendecastro@gmail.com>
2023-06-25 00:52:37 +0100

    about me section added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React from 'react';
 import Navbar from './components/navbar.js';
 import ContentHero from './components/content-hero.js';
 import Projects from './components/projects.js';
-
+import Aboutme from './components/aboutme.js';
 function App() {
   return (
 <>
       <Navbar />
       <ContentHero />
       <Projects />
+      <Aboutme />
 
 </>
 
---
+++ src/components/aboutme.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class aboutme extends Component {
+  render() {
+    return (
+        <section class="about-me" id="about">
+        <h1>About me </h1>
+        <div class="about-me-content">
+            <img src="assets/IMG_4070.PNG" alt="avatar-profile-picture"/>
+    
+            <div class="about-me-textbox">
+                <h2> Jayssen De Castro </h2>
+                <p>Lorem ipsum dolor sit amet. Qui sunt molestiae est dolor blanditiis qui quod provident cum ullam aperiam et provident voluptas? Rem atque galisum sed repellendus labore non error esse et labore magni eum voluptatem eveniet? Vel maiores deserunt quo blanditiis quam et similique quaerat ea obcaecati labore. Ab esse quas qui molestiae pariatur aut exercitationem officia</p>
+            </div>
+        </div>
+    </section> 
+    )
+  }
+}
