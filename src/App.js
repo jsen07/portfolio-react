d08295f08f665bcb52fecd1b3de2224b331c061a
unknown <jayssendecastro@gmail.com>
2023-06-25 00:41:15 +0100

    project section added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import Navbar from './components/navbar.js';
 import ContentHero from './components/content-hero.js';
+import Aboutme from './components/aboutme.js';
 
 function App() {
   return (
 <>
       <Navbar />
       <ContentHero />
+      <Aboutme />
 
 </>
 
---
+++ src/components/aboutme.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export default function Aboutme() {
+  return (
+    <section class="project" id="projects">
+        <h1> Projects </h1>
+    
+        <section class="project-container">   
+            <figure class="project-1">
+                <h2>Horiseon</h2>
+                <a href="https://jsen07.github.io/CodingBootcamp-Week1-Assignment1/" target="_blank">
+                <span class="link"></span></a> 
+            </figure>
+            
+            <div class="sub-container">
+                <figure class="project-2">
+                <h2> Meal Plan </h2>
+                <a href="https://abstynent.github.io/project1-meal-plan-app/" target="_blank">
+                <span class="link"></span></a> 
+            </figure>
+
+            <figure class="project-3">
+                <h2> Project 3 </h2>
+                <a href="https://readsky.herokuapp.com/" target="_blank">
+                    <span class="link"></span></a> 
+            </figure>
+        </div>
+            
+        <div class="sub-container-2">
+            <figure class="project-4">
+                <h2> Project 4 </h2>
+            </figure>
+            
+            <figure class="project-5">
+                <h2> Project 5 </h2>                        
+            </figure>
+        </div>
+    </section>
+</section>
+  )
+}
